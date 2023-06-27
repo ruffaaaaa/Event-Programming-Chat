2e2f59a5acba494d4771aac31007edfbaae3e7aa
ruffaaaaa <99468986+ruffaaaaa@users.noreply.github.com>
2023-06-26 18:04:11 -0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+const PORT = 3000 || process.env.PORT
+
+app.listen(PORT, () => console.log(`App is live on port ${PORT}`));
+
+
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')));
